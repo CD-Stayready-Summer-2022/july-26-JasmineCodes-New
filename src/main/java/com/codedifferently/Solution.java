da0b2aa1eebb8e0358f04ff24885a4510f6af1af
JasmineCodes-New <jasmine.irvin@ymail.com>
2022-07-26 09:33:19 -0400

    updates

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,18 @@
 package com.codedifferently;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
+        Map<String, Integer> letters = new TreeMap<>();
+
+        for (int i = 0; i < input.length(); i++) {
+        }
         return null;
     }
+
+    // count the characters in string
+    // treemap, maybe substring
+    // return it in a string "a:" +  a int count", "b:" + b int count, etc.
 }
